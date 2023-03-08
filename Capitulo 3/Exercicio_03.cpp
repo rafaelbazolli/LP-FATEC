e3cb31abea918c4471e672b20edc73231d835917
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-03-07 22:07:27 -0300

    Alteracoes nos exercicios 3 e 4

--- Capitulo 3/Exercicio_03.cpp
+++ Capitulo 3/Exercicio_03.cpp
@@ -6,11 +6,12 @@ main() {
 	/* Criar um programa (CALCULADORA ) em linguagem C que calcule as quatro operações básicas ( + ,- , / e * ). */
 	system("cls");
 	
-	int opcao;
+	int opcao, retornoMenu;
 	float num1, num2, resultado;
 	
+	menu:
 	printf("CALCULADORA\n");
-	printf("Operacoes:\n1- Adicao\n2- Subtracao\n3- Multiplicacao\n4- Divisao \nDigite a operacao desejada: \n");
+	printf("Operacoes:\n1- Adicao\n2- Subtracao\n3- Multiplicacao\n4- Divisao \n5 - SairDigite a operacao desejada: \n");
 	scanf("%i", &opcao);
 	
 	if(opcao > 4 || opcao < 1) {
@@ -18,6 +19,7 @@ main() {
 	}
 	else 
 	{
+		entradaValores:
 		printf("\nDigite o primeiro numero: ");
 		scanf("%f", &num1);
 		printf("\nDigite o segundo numero: ");
@@ -26,25 +28,58 @@ main() {
 	
 	
 	if(opcao == 1) {
+		soma:
 		resultado = num1 + num2;
 		printf("%.2f + %.2f = %.2f", num1, num2, resultado);
+		
+		printf("Deseja efetuar outra soma?(1- SIM / 2 - NAO)");
+			scanf("%i", &retornoMenu);
+			
+			if(retornoMenu == 1) {
+				goto soma;
+			}
 	}
 	else
 		if(opcao == 2) {
+			subtracao:
 			resultado = num1 - num2;
 			printf("%.2f - %.2f = %.2f", num1, num2, resultado);
+			
+			printf("Deseja efetuar outra subtracao?(1- SIM / 2 - NAO)");
+			scanf("%i", &retornoMenu);
+			
+			if(retornoMenu == 1) {
+				goto subtracao;
+			}
 		}
 		else
 			if(opcao == 3) {
+				multiplicacao:
 				resultado = num1 * num2;
 				printf("%.2f * %.2f = %.2f", num1, num2, resultado);
+				
+				printf("Deseja efetuar outra multiplicacao?(1- SIM / 2 - NAO)");
+				scanf("%i", &retornoMenu);
+			
+				if(retornoMenu == 1) {
+					goto multiplicacao;
+				}
 			}
 			else
 				if(opcao == 4) {
+					divisao:
 					resultado = num1 / num2;
 					printf("%.2f / %.2f = %.2f", num1, num2, resultado);
 				}
+				
+				printf("Deseja efetuar outra divisao?(1- SIM / 2 - NAO)");
+				scanf("%i", &retornoMenu);
 			
+				if(retornoMenu == 1) {
+					goto divisao;
+				}
+	
 	getch();
+	end:;
 }
 
--- Capitulo 3/Exercicio_04.cpp
+++ Capitulo 3/Exercicio_04.cpp
@@ -6,13 +6,14 @@ main() {
 	/* Criar tr�s programas em linguagem C que identifique e que ordene 2, 3 e 4 n�meros em ordem decrescente. */
 	system("cls");
 	
-	int quantidade;
+	int quant;
 	float num1, num2, num3, num4;
 	
 	printf("Programa para ordenar valores\n");
 	printf("Quantos valores voce deseja ordenar(2, 3 ou 4)? ");
-	scanf("%f", &quantidade);	
-	
-			
+	scanf("%f", &quant);
+
+	if (quant == 2)
+
 	getch();
 }
