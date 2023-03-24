f705850ef89baefb46c399c95ba8bf5026d7dafb
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-03-24 20:09:44 -0300

    Teste

--- Capitulo 4/Exercicio_15.cpp
+++ Capitulo 4/Exercicio_15.cpp
@@ -3,10 +3,10 @@
 #include<windows.h> // Responsavel pelo comando system
 
 main() {
-	/* Crie um programa em C, usando FOR, para receber as notas de 10 alunos, e ao final exibir quantos deles estão de recuperacao, e quantos ja estao aprovados.
+	/* Crie um programa em C, usando FOR, para receber as notas de 10 alunos, e ao final exibir quantos deles estï¿½o de recuperacao, e quantos ja estao aprovados.
 	Considerar que estao de recuperacao os alunos com notas inferiores a 6, e os aprovados com as notas iguais ou superiores a 6.  */
 	
 	
 	
-	getch();  // Aguarda o usuario digitar uma tecla qualquer para encerrar a execucao do programa
+	getch();  // Aguarda o usuario digitar uma tecla qualquer para encerrar a execucao do programa 
 }
