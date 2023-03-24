740c8a99f3de0683bebc7b92f7e8279263ed4038
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-03-24 20:07:23 -0300

    Teste de commit

---
+++ Capitulo 4/Exercicio_15.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h> // Responsavel pelos comandos printf e scanf
+#include<conio.h> // Responsavel pelo comando getch
+#include<windows.h> // Responsavel pelo comando system
+
+main() {
+	/* Crie um programa em C, usando FOR, para receber as notas de 10 alunos, e ao final exibir quantos deles estão de recuperacao, e quantos ja estao aprovados.
+	Considerar que estao de recuperacao os alunos com notas inferiores a 6, e os aprovados com as notas iguais ou superiores a 6.  */
+	
+	
+	
+	getch();  // Aguarda o usuario digitar uma tecla qualquer para encerrar a execucao do programa
+}
