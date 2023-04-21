a34c714a23e5a034a994f753746f25111e1cf62e
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-04-21 15:21:05 -0300

    correções no exercicio 05

--- Capitulo 5/Exercicio_05.cpp
+++ Capitulo 5/Exercicio_05.cpp
@@ -13,15 +13,8 @@ main() {
     printf("Digite uma palavra: ");  // Imprime a mensagem na tela
     scanf("%s", palavra);  // Recebe do usuario a palavra que sera exibida no loop
 
-    while (true) { // Loop infinito
-        printf("%s\n", palavra); // Imprime a palavra digitada pelo usuario
-
-        // A funcao kbhit captura qualquer tecla digitada pelo usuario
-        if (kbhit()) {  // Se a funcao kbhit receber algo, ou seja, se o usuario pressionar qualquer tecla, executa o bloco abaixo
-            break;  // O comando break intrerrompe a execucao do laco atual, ou seja, ele sai do laco while(linha 17 a 24)
-        }
-    }
-	
+    while(!kbhit()) // Loop infinito
+        printf("%s\n", palavra); // Imprime a palavra digitada pelo usuario	
 	getch();  // Aguarda o usuario digitar uma tecla qualquer para encerrar a execucao do programa
 }
 
