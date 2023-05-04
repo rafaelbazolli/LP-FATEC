23bba7fe92d102f48e74124dcbfe040319bfdecc
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-05-04 10:42:58 -0300

    Alterações nos exercicios 7 - 8

--- Capitulo 8/Exercicio_07.cpp
+++ Capitulo 8/Exercicio_07.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>   // Responsavel pelos comandos printf e scanf
+#include<math.h>  // Responsavel pelo comando sqrt
 
 int main() {
-    int matriz[3][3], novaMatriz[3][3], i, j;
-    
+    float matriz[4][3], novaMatriz[4][3];
+    int i, j;
+
+    // Recebendo os dados na matriz principal
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("Digite o elemento na posicao [%d][%d]: ", i, j);
+            scanf("%f", &matriz[i][j]);
+        }
+    }
+
+    // Gerando a nova matriz com a raiz quadrada dos numeros
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 3; j++) {
+            novaMatriz[i][j] = sqrt(matriz[i][j]);
+        }
+    }
+
+    // Exibindo a nova matriz 
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%.2f\t", novaMatriz[i][j]);
+        }
+        printf("\n");
+    }    
 }
---
+++ Capitulo 8/Exercicio_10.cpp
@@ -0,0 +1,24 @@
+// #include <stdio.h>   // Responsavel pelos comandos printf e scanf
+
+// /* 10 - Faça um programa usando matrizes para ajudar uma pizzaria a registrar e analisar alguns pedidos. O programa deve ler 12 pedidos, de acordo com os seguintes requisitos: 
+//     - O numero do pedido deve ser sequencial(1, 2, 3...), e nao precisa ser solicitado ao usuario;
+//     - Cada pedido deve conter apenas um tipo de pizza, a quantidade de pizzas, e o valor total;
+//     - Se o valor total do pedido passou de R$ 200,00, ele deve receber um desconto de 10% no valor total antes de ser armazenado;
+//     - Ao final, imprimir na tela o pedido de menor valor, o de maior valor, o valor total dos pedidos, a média do valor dos pedidos;
+//     - Imprimir também quantos pedidos foram das pizzas de calabresa, de mussarela, e de pepperoni. */
+
+// int main() {
+//     int i, j, pedidoEQuant[5][2];
+//     float total[5][1], maior, menor, valorTotal, mediaValores;
+//     char pizza[5][30];
+
+//     for(i = 0; i < 5; i++) {
+//         fflush(stdin);
+//         printf("\nPedido %d\n\n", i + 1);
+//         scanf("%d", &pedidoEQuant[i][0]);
+
+        
+
+//     }
+
+// }
