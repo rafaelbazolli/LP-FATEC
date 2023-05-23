98923ca1cba4f115257abcb66705892fd1abd430
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-05-23 18:22:01 -0300

    Commit Geral - 11-00

--- Maratona Programaçao/Maratonas anteriores/All/ProblemaC-Maratona2019.py
+++ Maratona Programaçao/Maratonas anteriores/All/ProblemaC-Maratona2019.py
@@ -1,37 +1,37 @@
-#Cruzamento perigoso
+# #Cruzamento perigoso
 
-posicaoVeiculos = []
-entrada = str(input())
-linhas, colunas, numVeiculos = map(int, entrada.split())
+# posicaoVeiculos = []
+# entrada = str(input())
+# linhas, colunas, numVeiculos = map(int, entrada.split())
 
-for veiculo in range(numVeiculos):
-    entrada = str(input())
-    posX, posY, direcao = entrada.split()
-    posX = int(posX); posY = int(posY)
+# for veiculo in range(numVeiculos):
+#     entrada = str(input())
+#     posX, posY, direcao = entrada.split()
+#     posX = int(posX); posY = int(posY)
     
-    if(direcao == 'N'):
-        aux = []
-        aux.append([posX, posY])
-        for i in range(posY, 0):
-            aux.append([posX, i])
-        posicaoVeiculos.append(aux)
-    elif(direcao == 'S'):
-        aux = []
-        aux.append([posX, posY])
-        for i in range(posY, linhas):
-            aux.append([posX, i])
-        posicaoVeiculos.append(aux)
-    elif(direcao == 'L'):
-        aux = []
-        aux.append([posX, posY])
-        for i in range(posX, colunas):
-            aux.append([posX, i])
-        posicaoVeiculos.append(aux)
-    elif(direcao == 'O'):
-        aux = []
-        aux.append([posX, posY])
-        for i in range(posX, 0):
-            aux.append([posX, i])
-        posicaoVeiculos.append(aux)
-print(posicaoVeiculos)
+#     if(direcao == 'N'):
+#         aux = []
+#         aux.append([posX, posY])
+#         for i in range(posY, 0):
+#             aux.append([posX, i])
+#         posicaoVeiculos.append(aux)
+#     elif(direcao == 'S'):
+#         aux = []
+#         aux.append([posX, posY])
+#         for i in range(posY, linhas):
+#             aux.append([posX, i])
+#         posicaoVeiculos.append(aux)
+#     elif(direcao == 'L'):
+#         aux = []
+#         aux.append([posX, posY])
+#         for i in range(posX, colunas):
+#             aux.append([posX, i])
+#         posicaoVeiculos.append(aux)
+#     elif(direcao == 'O'):
+#         aux = []
+#         aux.append([posX, posY])
+#         for i in range(posX, 0):
+#             aux.append([posX, i])
+#         posicaoVeiculos.append(aux)
+# print(posicaoVeiculos)
     
---
+++ Maratona Programaçao/Maratonas anteriores/All/ProblemaE-Maratona2019.py
@@ -0,0 +1,5 @@
+
+# Problema E - Exibição de Peixes
+# Pegar sempre do aquario que tem menos fêmeas e transferi-las para o com mais femeas.
+# Assim que zerar as fêmeas de um aquario, no proximo dia ja vai ter mais uma femea. Aí movemos ela pra outro aquario
+# Só um aquario será usado pra transformar os peixes. Zerou as fêmeas, movemos os machos pra lá. 
