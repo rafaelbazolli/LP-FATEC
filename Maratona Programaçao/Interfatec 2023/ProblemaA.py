c2a9e90203c13d19d20814133d41635bae0f5e4f
Rafael Bazolli <rafael_sts1@hotmail.com>
2023-06-17 00:03:11 -0300

    Alteração Geral - 13-00

--- Maratona Programaçao/Interfatec 2023/ProblemaA.py
+++ Maratona Programaçao/Interfatec 2023/ProblemaA.py
@@ -1,14 +1,16 @@
-
-livros = []; contagem = 0
+contagem = 0; livros = []
 qtdLivros = int(input())
-livrosFinal = str(input())
+livrosFinal = list(input())
 
-livrosOrdenado = sorted(livrosFinal)
-
-while livrosFinal != livrosOrdenado:
-    for i in range(qtdLivros - 1):
-        if livrosFinal[i] > livrosFinal[i+1]:
-            livrosFinal = livrosFinal[:i] + livrosFinal[i+1] + livrosFinal[i] + livrosFinal[i+2:]
+for i in range(qtdLivros - 1): 
+    for j in range(qtdLivros - i - 1): # A cada iteração, o range diminui, pois já terá sido validado um livro(da direita pra esquerda)
+        if livrosFinal[j] > livrosFinal[j + 1]:  # Se o anterior for maior que o próximo, inverte a posição
+            livros.append(livrosFinal[j])   # Armazena em livros, qual livro foi trocado de lugar(qual foi lido)
+            livrosFinal[j], livrosFinal[j + 1] = livrosFinal[j + 1], livrosFinal[j]
             contagem += 1
 
-print(contagem)
+# Validando se algum estudante pegou livro mais de 5x
+repeticoes = [livros.count(livro) for livro in livros] # Conta quantas vezes cada livro foi movido
+estudanteDBPegou = any(frequencia > 5 for frequencia in repeticoes)
+
+print('A Database Systems student read a book.' if (estudanteDBPegou) else contagem)
